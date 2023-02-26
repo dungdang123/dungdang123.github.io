04dc040866a68a9b445453bb215d0b75695fdebd
Dung Tom <58303777+dungdang123@users.noreply.github.com>
2023-02-26 23:03:49 +0700

    Update script.js
    Signed-off-by: Dung Tom <58303777+dungdang123@users.noreply.github.com>

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// Copyright belongs to Dang Van Dung. The copies need to be notified in advance. Thank you.
 let d = new Date();
 const birthdays = 2004;
 const years = d.getFullYear();
@@ -14,47 +15,6 @@ console.log(dayname[dayofweek] + ' ngày '+ timeLert(day) + '/' + timeLert(month
 setTimeout(function() {
     $(".reposnt").fadeOut(1700);
 }, 2000)
-
-fetchData();
-function fetchData() {
-    const data = [
-        {
-            "link": "https://github.com/dungdang123",
-            "backgound": "background-color: #0093E9;background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);",
-            "image": "./public/platforms/github.svg",
-            "username": "dungdang123"
-        }, {
-            "link": "https://twitter.com/mecstomk",
-            "backgound": "background-color: #4158D0;background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);",
-            "image": "./public/platforms/twitter.svg",
-            "username": "@mecstomk"
-        }, {
-            "link": "https://facebook.com/dangvandung.dev/",
-            "backgound": "background-color: #FAD961;background-image: linear-gradient(90deg, #FAD961 0%, #F76B1C 100%);",
-            "image": "./public/platforms/facebook.svg",
-            "username": "@dangvandung.dev"
-        }, {
-            "link": "#",
-            "backgound": "background-color: #FA8BFF;background-image: linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%);",
-            "image": "./public/platforms/riotgames.svg",
-            "username": "@dungtomxanh"
-        }, {
-            "link": "#",
-            "backgound": "background-color: #8EC5FC;background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);",
-            "image": "./public/icons/4834215.png",
-            "username": "@dungmecstomk"
-        }
-    ];
-
-    // data.forEach(obj => {
-    //     Object.entries(obj).forEach(([key, value]) => {
-    //         console.log(`${key} ${value}`);
-    //         const pathMain = ``;
-    //         let showSocian = document.querySelector("");
-    //         showSocian.append(pathMain)
-    //     });
-    // });
-}
 
 function timeLert(i) {
     let string = "";
